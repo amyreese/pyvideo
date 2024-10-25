d367d2fbaffffcc83dfdd753e8909fc5fabcddf6
Amethyst Reese <amethyst@n7.gg>
2024-10-24 22:26:54 -0700

    Make things work on Amy's machine

--- plugins/writer.py
+++ plugins/writer.py
@@ -12,6 +12,7 @@ class PyVideoWriter(Writer):
     """
     This class exists so that elements in a feed can be sorted.
     """
+
     def write_feed(self, elements, *args, **kwargs):
         elements = self.sort_elements(elements)
         return super().write_feed(elements, *args, **kwargs)
@@ -21,7 +22,7 @@ class PyVideoWriter(Writer):
 
     def key_func(self, element):
         path = element.data_path  # eg. pydata-dc-2016/videos/closing-session.json
-        event_dir = path.split('/')[0]
+        event_dir = path.split("/")[0]
         if event_dir not in self.sorted_events:
             return len(self.sorted_events)
         else:
@@ -30,18 +31,21 @@ class PyVideoWriter(Writer):
 
     @property
     def sorted_events(self):
-        if not hasattr(self, '_sorted_events'):
+        if not hasattr(self, "_sorted_events"):
             events = []
             old_wd = os.getcwd()
-            os.chdir('data')
-            for path in glob.iglob('**/category.json'):
+            os.chdir("data")
+            for path in glob.iglob("**/category.json"):
                 directory = os.path.dirname(path)
                 command = "git log --diff-filter=A --follow --format=%ai -1 --".split()
                 command.append(path)
-                output = subprocess.check_output(
-                    command, universal_newlines=True)
-                dt = datetime.datetime.strptime(
-                    output.strip(), "%Y-%m-%d %H:%M:%S %z")
+                output = subprocess.check_output(command, universal_newlines=True)
+                try:
+                    dt = datetime.datetime.strptime(
+                        output.strip(), "%Y-%m-%d %H:%M:%S %z"
+                    )
+                except ValueError as e:
+                    raise ValueError("No git log available for %s" % directory) from e
                 events.append((dt, directory))
             os.chdir(old_wd)
             events.sort(key=operator.itemgetter(1))
